98beed7c6259aac007a465adaac12627ea05ca41
Darwin-Joel-Cuchipe-Chavez <djcuchipe@espe.edu.ec>
2025-06-04 08:45:15 -0500

    Add files via upload

--- U2/VECTORES/CUCHIPE DARWIN/Norma del Cuadrado.c
+++ U2/VECTORES/CUCHIPE DARWIN/Norma del Cuadrado.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
-#define MAX 10
 
 float vec[];
-float num, suma;
-int i;
-void invector (float vec[], int n){
-    for(int i=1; i<=MAX; i++){
-        printf("Ingresar un nÃºmero para la posiciÃ³n %d:",i);
-        scanf("%f",&num);
-        vec[i] = num*num;
-        suma = suma + vec[i];
-        }
-    printf("El cuadrado del vector es: %f",suma);
-}
+float num, suma = 0;
+int i, lim;
+//void invector (float vec[], int n){
+    //lim = 1;
+//}
 
 int main (){
-    invector(vec,MAX);
+    printf("INGRESAR UNA CANTIDAD QUE VA A SER EL TAMAÑO MAXIMO DEL VECTOR (MAXIMO 10): \n");
+    scanf("%i",&lim);
+    if (lim>0 && lim<11){
+        for(int i=1; i<=lim; i++){
+            printf("Ingresar un número para la posición %d:\n",i);
+            scanf("%f",&num);
+            vec[i] = num*num;
+            suma = suma + vec[i];
+            printf("El cuadrado del vector es: %.2f\n",suma);
+        }
+    }
+    else{
+        printf("ERROR, NO HAZ INGRESADO UNA CANTIDAD VALIDA :C");
+    }
     return 0;
 }
